6456f40eb449758b82a111b0becdecd6bdb56255
arranoust <69747304+arranoust@users.noreply.github.com>
2026-02-24 06:12:01 +0700

    9

--- app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
+++ app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
@@ -2031,15 +2031,6 @@ class MainActivity : AppCompatActivity(), ColorPickerDialogListener, BiometricCa
         try {
             if (getKey(HAS_DONE_SETUP_KEY, false) != true) {
                 navController.navigate(R.id.navigation_setup_language)
-                // If no plugins bring up extensions screen
-            } else if (PluginManager.getPluginsOnline().isEmpty()
-                && PluginManager.getPluginsLocal().isEmpty()
-//                && PREBUILT_REPOSITORIES.isNotEmpty()
-            ) {
-                navController.navigate(
-                    R.id.navigation_setup_extensions,
-                    SetupFragmentExtensions.newInstance(false)
-                )
             }
         } catch (e: Exception) {
             logError(e)
